dd8cd483d9406cf3ac08a346f1683bbbd047dfa5
Gabriela <mr27612@gmail.com>
2026-06-03 21:49:27 -0300

    arquivo inicial do bot

---
+++ bot.py
@@ -0,0 +1,50 @@
+# Autor: Gabriela Souza
+# Projeto bot de telegram com raspagem de dados
+
+
+# Passos
+
+# Capturar a info
+# Só me notifique se chegar na minha condição
+
+
+# importações
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import time
+
+# informações do telegram
+TOKEN = 'SEU_TOKEN'
+CHAT_ID = 'SEU_ID'
+
+# url do site
+url = "https://www.americanas.com.br/tv-43--tcl-full-hd-qled-43s5k-google-tv-2-hdmi-8296741/p"
+
+# driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get(url) # mostra as informações da url
+
+time.sleep(5) # tempo que a tela do ecommerce fica aberta
+
+# classe dinamica
+preco = driver.find_element(
+    By.CSS_SELECTOR, "[class*='ProductPrice_productPrice']").text # raspa os dados da classe
+    # By.CSS_SELECTOR -> seleciona classe com css dinamico
+    # By.CLASS_NAME -> seleciona classe estatica
+
+# exibir os resultados
+print(preco)
+
+mensagem = f"preço encontrado: {preco}\n{url}"
+
+requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": mensagem
+    })
+
+driver.quit()
